37bf610bc88709714dba474e88775cdf64bc7ee5
hyunsik <shs9589@gmail.com>
2018-03-18 02:04:01 +0900

    set up finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import styles from './styles.scss'
 
-export default () => <div className={styles.title}>lalalala</div>
+export default () => (
+    <div className = {styles.title}>
+        <h1>lorem</h1>
+        <p>lorem ipsum </p>
+    </div>
+);
